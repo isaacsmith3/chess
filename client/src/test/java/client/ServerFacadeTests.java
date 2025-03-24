985fa5af99c04a3577186dc1734ff360b9c2ce6b
Isaac Smith <isaacwallacesmith@gmail.com>
2025-03-24 13:26:21 -0600

    begin tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,33 @@
 package client;
 
+import exception.ResponseException;
 import org.junit.jupiter.api.*;
+import server.PreLoginClient;
 import server.Server;
+import server.ServerFacade;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static PreLoginClient client;
+    private static String serverUrl;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(5051);
+        serverUrl = "http://localhost:" + port;
         System.out.println("Started test HTTP server on " + port);
+        client = new PreLoginClient(serverUrl);
+
+        try {
+            new ServerFacade(serverUrl).clear();
+        } catch (ResponseException e) {
+            System.out.println("Error clearing database: " + e.getMessage());
+        }
     }
 
     @AfterAll
@@ -20,10 +35,22 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clearDatabase() {
+        try {
+            new ServerFacade(serverUrl).clear();
+        } catch (ResponseException e) {
+            System.out.println("Error clearing database: " + e.getMessage());
+        }
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void testHelpCommandPositive() {
+        String result = client.help();
+        assertTrue(result.contains("Help Menu"));
+        assertTrue(result.contains("register"));
+        assertTrue(result.contains("login"));
+        assertTrue(result.contains("quit"));
     }
 
 }
